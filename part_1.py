a853df04d2fb7e2f75f60acc3d6508f4d71a921f
maja-ja <134922426+maja-ja@users.noreply.github.com>
2025-07-17 15:18:09 +0800

    Update part_1.py

--- part_1.py
+++ part_1.py
@@ -1,31 +1,39 @@
 # FNN_part_1.py
 # This code snippet is a part of learning of machine learning by maja-ja, a github noob.
-# ----------- simple functin y=w*x+b ----------------
+# ----------- simple function y = w * x + b ----------------
 # this function is used to calculate the output of a simple linear model.
 # w and b can change, but x is fixed.
 # if we let theta = [w, b], then we can write the function as:
-# f_theta(x) = w*x + b, forexample:
+# f_theta(x) = w*x + b, for example:
 # f_theta(3) = 2*x + 1, theta = [2, 1]
 # ----------- loss function ------------------------
-# normally, the loss function is used to measure how well the model performs.
-# L= sigma((y[i]-y_hat[i])^2)    
+# Normally, the loss function is used to measure how well the model performs.
+# For regression tasks, one common loss is the Mean Squared Error (MSE):
+# L = Σ((y[i] - y_hat[i])^2) / n
 # where:
-# (y[i]-y_hat[i]) = error between the actual output y[i] and the predicted output y_hat[i]
-# this function is used to calculate the error for each input sample
-# in python, this can be implemented as:
-# L = sum((y[i] - y_hat[i])**2 for i in range(len(y)))
-# L = sum((a - b)**2 for a, b in zip(y, y_hat)) #more pythonic way
+#   - y[i] is the actual value
+#   - y_hat[i] is the predicted value
+#   - (y[i] - y_hat[i])^2 is the squared error for each sample
+# In Python, this can be implemented as:
+
+L = sum((a - b)**2 for a, b in zip(y, y_hat)) / len(y)  # Mean Squared Error
 # ----------- function of learning ------------------
-# 3                                                       <--\  
-# sigma(x[i]*w[i])+b[1]=w[1]*x[1] + w[2]*x[2] + w[3]*x[3] <---- activation function
-# where:                                                  <--/
-# x[i]=input values   # can't change = fixed inputs
-# w[i]=input weights  # can change 
-# b[1]=bias           # can change
-# sigma=activation function
-# w[i] and b[1] are the parameters of the model that we want to learn
-# the goal of learning is to find the best values for w[i] and b[1] that minimize the loss function L
-#------------ how to learning -------------------
+# For a simple neural unit (neuron), the output is calculated as:
+# a = σ(w[1]*x[1] + w[2]*x[2] + w[3]*x[3] + b)
+# or more generally:
+# a = σ(Σ(w[i]*x[i]) + b)       # using summation
+# or:
+# a = σ(w · x + b)              # using vector dot product
+
+# where:
+#   - x[i]: input values (fixed during training)
+#   - w[i]: weights (trainable parameters)
+#   - b: bias term (trainable parameter)
+#   - σ: activation function (e.g., sigmoid, ReLU)
+
+# The goal of learning is to find the best values for w and b 
+# that minimize the loss function L across training samples.
+#------------ how makes machine learning -------------------
 # simple learning function y=w*x+b in python can be implemented as:
 def linear_model(x, theta):
     w, b = theta
@@ -33,7 +41,7 @@ def linear_model(x, theta):
 
 x_values = [1, 2, 3] # input values
 y_true = [3, 5, 7]   # output values
-theta = [2, 1]       # w=2, b=1, w=2, b=1, # if we want y = 2x + 1
+theta = [2, 1]       # w=2, b=1 if we want y = 2x + 1
 
 y_hat = [linear_model(x, theta) for x in x_values]
 loss = sum((y - y_pred)**2 for y, y_pred in zip(y_true, y_hat))
@@ -45,4 +53,4 @@ theta = [2, 1]      # w=2, b=1, # if we want y = 2x + 1
 
 y_hat = [linear_model(x, theta) for x in x_values]
 loss = sum((y - y_pred)**2 for y, y_pred in zip(y_true, y_hat))
-print(f"Loss: {loss}") # los shonld'n be 0, because the model is not perfect for these inputs
+print(f"Loss: {loss}") # loss shouldn't be 0, because the model is not perfect for these inputs
