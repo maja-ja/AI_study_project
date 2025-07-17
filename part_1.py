8ef2c11628b70720423f6dcc646033c2cbb0f1c6
maja-ja <134922426+maja-ja@users.noreply.github.com>
2025-07-17 15:24:43 +0800

    Update part of part_1.py

--- part_1.py
+++ part_1.py
@@ -1,11 +1,11 @@
-# FNN_part_1.py
+# part_1.py
 # This code snippet is a part of learning of machine learning by maja-ja, a github noob.
 # ----------- simple function y = w * x + b ----------------
 # this function is used to calculate the output of a simple linear model.
 # w and b can change, but x is fixed.
 # if we let theta = [w, b], then we can write the function as:
 # f_theta(x) = w*x + b, for example:
-# f_theta(3) = 2*x + 1, theta = [2, 1]
+# f_theta(3) = 2*3 + 1 = 7, theta = [2, 1]
 # ----------- loss function ------------------------
 # Normally, the loss function is used to measure how well the model performs.
 # For regression tasks, one common loss is the Mean Squared Error (MSE):
@@ -15,8 +15,9 @@
 #   - y_hat[i] is the predicted value
 #   - (y[i] - y_hat[i])^2 is the squared error for each sample
 # In Python, this can be implemented as:
-
-L = sum((a - b)**2 for a, b in zip(y, y_hat)) / len(y)  # Mean Squared Error
+loss = sum((y - y_hat)**2 for y, y_hat in zip(y_true, y_hat)) / len(y_true)
+# or:
+loss = sum((y - y_pred)**2 for y, y_pred in zip(y_true, y_pred)) / len(y_true) # Mean Squared Error
 # ----------- function of learning ------------------
 # For a simple neural unit (neuron), the output is calculated as:
 # a = σ(w[1]*x[1] + w[2]*x[2] + w[3]*x[3] + b)
@@ -44,13 +45,13 @@ y_true = [3, 5, 7]   # output values
 theta = [2, 1]       # w=2, b=1 if we want y = 2x + 1
 
 y_hat = [linear_model(x, theta) for x in x_values]
-loss = sum((y - y_pred)**2 for y, y_pred in zip(y_true, y_hat))
+loss = sum((y - y_hat)**2 for y, y_hat in zip(y_true, y_hat))
 print(f"Loss: {loss}") # loss should be 0 if the model is perfect
 
 x_values = [1, 3, 5] # input values, changed to test the model
 y_true = [3, 5, 7]   # output values, changed to test the model
-theta = [2, 1]      # w=2, b=1, # if we want y = 2x + 1
+theta = [2, 1]      # w=2, b=1 if we want y = 2x + 1
 
 y_hat = [linear_model(x, theta) for x in x_values]
-loss = sum((y - y_pred)**2 for y, y_pred in zip(y_true, y_hat))
+loss = sum((y - y_hat)**2 for y, y_hat in zip(y_true, y_hat))
 print(f"Loss: {loss}") # loss shouldn't be 0, because the model is not perfect for these inputs
